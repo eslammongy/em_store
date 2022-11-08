cff7e9977c12c454427fbf82a8846285a84dfeec
eslam <eslammongy995@gmail.com>
2022-11-08 13:49:34 +0200

    adding app colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color textColor = Color(0XFFccc7c5);
+  static const Color mainColor = Color(0XFF89dad0);
+  static const Color iconsColor = Color(0XFFffd28d);
+  static const Color buttonBGColor = Color(0XFFf7f6f4);
+  static const Color titleColor = Color(0XFF5c524f);
+  static const Color mainBlackColor = Color(0XFF2b2b2b);
+  static const Color mainOrangeColor = Color(0XFFccc7c5);
+}
