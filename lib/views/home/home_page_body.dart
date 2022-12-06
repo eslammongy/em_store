856eb501a3bd84a4abd35d6eada4307098c7c229
eslam <eslammongy995@gmail.com>
2022-12-07 01:01:27 +0200

    build a create transform function

--- lib/views/home/home_page_body.dart
+++ lib/views/home/home_page_body.dart
@@ -13,9 +13,32 @@ class HomePageBody extends StatefulWidget {
 
 class _HomePageBodyState extends State<HomePageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue = 0.0;
+  final double _scaleFactor = 0.8;
+  final _cardHeight = 220;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      pageController.addListener(() {
+        setState(() {
+          _currentPageValue = pageController.page!;
+          //print("Current Page Value:: $_currentPageValue");
+        });
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 320,
       child: PageView.builder(
           controller: pageController,
@@ -27,86 +50,123 @@ class _HomePageBodyState extends State<HomePageBody> {
   }
 
   Widget _buildPageItem(int position) {
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: const EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              /*  color: position.isEven ? Colors.amber[900] : Colors.purple[300], */
-              image: const DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/pexels_carollo.jpg'))),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 140,
-            margin: const EdgeInsets.only(left: 15, right: 15, bottom: 10),
+    return Transform(
+      transform: createMatrixTransform(position),
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: const EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
-            ),
+                borderRadius: BorderRadius.circular(30),
+                /*  color: position.isEven ? Colors.amber[900] : Colors.purple[300], */
+                image: const DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/pexels_carollo.jpg'))),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Container(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    HeadLineText(text: "Sample Head Text"),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Wrap(
-                          children: List.generate(
-                              5,
-                              (index) => const Icon(
-                                    Icons.star,
-                                    color: AppColors.mainColor,
-                                    size: 15,
-                                  )),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        SmallBodyText(text: '4.5'),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        SmallBodyText(text: '128 Comments')
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: const [
-                        IconAndTextWidget(
-                            icon: Icons.circle,
-                            color: AppColors.mainColor,
-                            text: 'normal'),
-                        IconAndTextWidget(
-                            icon: Icons.location_on,
-                            color: Colors.green,
-                            text: '36.6 km'),
-                        IconAndTextWidget(
-                            icon: Icons.alarm_rounded,
-                            color: AppColors.mainColor,
-                            text: '24 min'),
-                      ],
-                    )
-                  ]),
+              height: 140,
+              margin: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      HeadLineText(text: "Sample Head Text"),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Wrap(
+                            children: List.generate(
+                                5,
+                                (index) => const Icon(
+                                      Icons.star,
+                                      color: AppColors.mainColor,
+                                      size: 15,
+                                    )),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          SmallBodyText(text: '4.5'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          SmallBodyText(text: '128 Comments')
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: const [
+                          IconAndTextWidget(
+                              icon: Icons.circle,
+                              color: AppColors.mainColor,
+                              text: 'normal'),
+                          IconAndTextWidget(
+                              icon: Icons.location_on,
+                              color: Colors.green,
+                              text: '36.6 km'),
+                          IconAndTextWidget(
+                              icon: Icons.alarm_rounded,
+                              color: AppColors.mainColor,
+                              text: '24 min'),
+                        ],
+                      )
+                    ]),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
+  }
+
+  Matrix4 createMatrixTransform(int position) {
+    Matrix4 matrix4 = Matrix4.identity();
+
+    if (position == _currentPageValue.floor()) {
+      var currentScale =
+          1 - (_currentPageValue - position) * (1 - _scaleFactor);
+      var currentTrans = _cardHeight * (1 - currentScale) / 2;
+      matrix4 = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, currentTrans, 0);
+    } else if (position == _currentPageValue.floor() + 1) {
+      var currentScale = _scaleFactor +
+          (_currentPageValue - position + 1) * (1 - _scaleFactor);
+      var currentTrans = _cardHeight * (1 - currentScale) / 2;
+
+      matrix4 = Matrix4.diagonal3Values(1, currentScale, 1);
+      matrix4 = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, currentTrans, 0);
+    } else if (position == _currentPageValue.floor() - 1) {
+      var currentScale =
+          1 - (_currentPageValue - position) * (1 - _scaleFactor);
+      var currentTrans = _cardHeight * (1 - currentScale) / 2;
+
+      matrix4 = Matrix4.diagonal3Values(1, currentScale, 1);
+      matrix4 = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, currentTrans, 0);
+    } else {
+      var currentScale = 0.8;
+      matrix4 = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, _cardHeight * (1 - currentScale) / 2, 1);
+    }
+
+    return matrix4;
   }
 }
